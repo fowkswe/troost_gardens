1113e6bfc5c4753e57db853eb979607a5b37e65b
Billy Fowks <fowkswe@gmail.com>
2023-01-16 14:12:11 -0600

    ways of magic qr page

---
+++ pages/exhibitions/the-ways-of-magic/qr.vue
@@ -0,0 +1,10 @@
+<template lang='pug'>
+  .sunshine-and-shadow
+    h1 The Ways of Magic
+    a(href='/press-releases/The-Ways-of-Magic.pdf') The Ways of Magic
+</template>
+<script lang='coffee'>
+export default
+  mounted: ->
+    location.href = '/press-releases/The-Ways-of-Magic.pdf'
+</script>
