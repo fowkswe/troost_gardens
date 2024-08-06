d6dd532596a840e6e48468f2b1a105b2f84de0f8
fowkswe <fowkswe@gmail.com>
2024-08-06 08:12:31 -0500

    make it work

--- pages/sworks/_id.vue
+++ pages/sworks/_id.vue
@@ -1,18 +1,70 @@
 <template lang="pug">
-  .sworks
-
+.sworks
+  .mb-5
+    b-button(:to="'/sworks'" variant='outline-primary')
+      BIconArrowLeft.mr-2
+      | Back to Artworks
+  b-row
+    b-col(cols=12 md=4)
+      .mb-5
+        h1 {{ artwork.title }}
+        h2 {{ artistName }}
+        a(:href='artwork.artist.website' target='_blank' v-if='artwork.artist && artwork.artist.website') View Artists Website
+      .mb-3
+        p {{ artwork.medium.title }}
+        p {{ artwork.dimensions }}
+        p {{ artwork.description }}
+        h3 ${{ artwork.price }}
+      b-button.mb-3(:href='mailto' variant='primary')
+        | Inquire
+    b-col(cols=12 md=8)
+      b-img.w-100(:src='urlFor(artwork.image)')
 </template>
-<script lang="ts">
-import { useSanityClient } from 'vue-sanity'
+<script lang="js">
+import { BIconArrowLeft } from 'bootstrap-vue'
+import { client, urlFor } from '../../lib/sanity.ts'
 
 export default {
-  name: 'App',
-  setup () {
-    useSanityClient({
-      projectId: 'gwsyyifo',
-      dataset: 'production',
-      useCdn: process.env.NODE_ENV === 'production'
-    })
+  components: { BIconArrowLeft },
+  head () {
+    return {
+      title: this.artwork.title,
+      meta: [
+        { property: 'og:title', content: this.artwork.title },
+        { hid: 'description', name: 'description', content: this.artwork.description },
+        { property: 'og:description', content: this.artwork.description },
+        { property: 'og:image', content: urlFor(this.artwork.image) },
+        { property: 'og:url', content: `https://www.troostgardens.com${this.$route.path}` },
+        { property: 'og:site_name', content: 'Troost Gardens - Kansas City Art' },
+        { hid: 'twitter:title', name: 'twitter:title', content: this.artwork.title },
+        { hid: 'twitter:description', name: 'twitter:description', content: this.artwork.description },
+        { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+        { hid: 'twitter:image', name: 'twitter:image', content: urlFor(this.artwork.image) }
+      ]
+    }
+  },
+  computed: {
+    mailto () {
+      return `mailto:hi@troostgardens.com?subject=I would like to know more about ${this.artistName} - ${this.artwork.title}`
+    },
+    artistName () {
+      return this.artwork.artist ? this.artwork.artist?.full_name : 'Unknown Artist'
+    }
+  },
+  async asyncData (context) {
+    const query = "*[slug.current == '" + context.route.params.id +
+                  "']{_id,slug,title,dimensions,description,medium->{title},price,artist->{full_name, website},image}[0]"
+
+    const artwork = await client.fetch(query)
+
+    return { artwork }
+  },
+  methods: {
+    urlFor
   }
 }
 </script>
+<style lang='sass' scoped>
+ximg
+  width: 100%
+</style>
--- pages/sworks/index.vue
+++ pages/sworks/index.vue
@@ -2,32 +2,36 @@
 .sworks_index
   h1 Artworks for sale
   b-row
-    b-col(col=3 v-for="artwork in artworks" :key="artwork._id")
-      router-link(:to="`/sworks/${artwork.slug}`")
-        pre {{ artwork }}
-        h2 {{artwork.title}}
-        h3 {{artwork.artist.full_name}}
-        img(:src='urlFor(artwork.image)')
+    b-col(cols=12 md=4 v-for="artwork in artworks" :key="artwork._id")
+      router-link(:to="`/sworks/${artwork.slug.current}`")
+        b-img.mb-2(:src='urlFor(artwork.image).width(400).url()')
+        .d-flex.justify-content-between
+          .info.mb-4
+            h4 {{artwork.title}}
+            h5 {{artistName(artwork)}}
+          BIconArrowRight
 
 </template>
-<script lang="ts">
+<script lang="js">
+import { BIconArrowRight } from 'bootstrap-vue'
 import { client, urlFor } from '../../lib/sanity.ts'
 
 export default {
+  components: { BIconArrowRight },
   async asyncData (context) {
     const artworks = await client.fetch("*[_type == 'artwork']{_id,slug,title,dimensions,medium,price,artist->{full_name},image}")
 
     return { artworks }
   },
   methods: {
-    urlFor
+    urlFor,
+    artistName (artwork) {
+      return artwork.artist ? artwork.artist.full_name : 'Unknown Artist'
+    }
   }
-  // async setup () {
-  //   const { data: artworks } = await client.fetch('*[_type == "artwork"]')
-  //   console.log('artworks', artworks)
-
-  //   // const { data: artworks } = useSanityFetcher('*[_type == "artwork"]{title,"imageUrl": image.asset->url}')
-  //   return { artworks }
-  // }
 }
 </script>
+<style lang='sass' scoped>
+img
+  width: 100%
+</style>
